d82b6cb2363798ffbef05299c5d7b853e240a806
Bobby Rogers <rrogers82@gmail.com>
2013-07-04 15:10:25 -0400

    Added ZF and changed project to work with it on regular request

---
+++ public/phpinfo.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rrogers
+ * Date: 7/4/13
+ * Time: 10:49 AM
+ * To change this template use File | Settings | File Templates.
+ */
+phpinfo();
